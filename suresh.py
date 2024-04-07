dacb25dcb8556c3c4f806e7c0b85f1d0fc3fa364
RaghavIndana <165910950+RaghavIndana@users.noreply.github.com>
2024-04-07 16:29:21 +0530

    Add files via upload

---
+++ suresh.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import requests
+from PIL import Image
+from io import BytesIO
+
+st.title('Surendranath Benarjee')
+st.write('About Suresh')
+st.header('Vankaya')
+image ="brinjal.jpeg"
+st.image(image, caption='Image', use_column_width=True)
+
